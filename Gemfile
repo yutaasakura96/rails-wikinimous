d9aa4759ec23510b097d6fb60c53ee7708d16fbc
yuta asakura <yuta.asakura.se@gmail.com>
2024-08-06 16:21:21 +0900

    finished excercise

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.4"
 
-gem 'faker'
+gem 'faker', "~> 3.4.2"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n\n")
+  )
+end
